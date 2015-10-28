60acb7afe6fd8474f84c1dd90bb4927bf90869dc
farukkhan <faruk.cse14@gmail.com>
2015-10-28 18:07:58 +0600

    Widget Corp (core php);

---
+++ includes/layouts/footer.php
@@ -0,0 +1,12 @@
+	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="stylesheets/public.css" media="all" type="text/css">
+</head>
+<body>
+<div id="footer">Copyright © 2015, Widget Corp</div>
+</body>
+</html>
---
+++ includes/layouts/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="stylesheets/public.css" media="all" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<h1>Widget Corp</h1>	
+</body>
+</html>		
